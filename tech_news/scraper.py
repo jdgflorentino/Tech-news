38055fb958e38d0519b5f0e060d250c3d9b43bd3
Jéssica Florentino <jdgflorentino@gmail.com>
2022-11-09 17:16:19 -0300

    feat: 3 - Crie a função scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -26,7 +26,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    scrape = selector.css(".next::attr(href)").get()
+    return scrape
 
 
 # Requisito 4
